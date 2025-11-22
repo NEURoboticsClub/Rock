d1ab6e32d2bb05af54918d8ce11307bc4f4056cc
Jay-Atal <46697722+Jay-Atal@users.noreply.github.com>
2025-11-22 09:32:34 -0500

    Comp changes.

--- src/main.cpp
+++ src/main.cpp
@@ -58,98 +58,50 @@ void opcontrol() {
 	robotInit();
 
 	tankdrive.initialize(config.drivebase.driveStyle);
-	intake.initialize();
-	centerStageLower.initialize();
-	centerStageUpper.initialize();
-	hood.initialize();
-	intakeLeft.initialize();
-	intakeRight.initialize();
-	scoring.initialize();
+	while (true) {
+	pros::MotorGroup* intake = new pros::MotorGroup({-2, 10, -13, 21, -6});
+	pros::MotorGroup* scoring = new pros::MotorGroup({-1});
+		if(ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+			intake->move(127);
+		} else if (ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+			intake->move(-127);
+		} else {
+			intake->brake();
+		}
 
+		if (ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+			scoring->move(127);
+		} else if (ctrl.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+			scoring->move(-127);
+		} else {
+			scoring->brake();
+		}
 
-	// // // left auto
-	// Pose* startPose = new Pose(0.0, 0.0, M_PI/4.0);
-	// odom.setPose(startPose);
-	// delete startPose;
+		pros::delay(50);
+	}
+	// intake.initialize();
+	// centerStageLower.initialize();
+	// centerStageUpper.initialize();
+	// hood.initialize();
+	// intakeLeft.initialize();
+	// intakeRight.initialize();
+	// scoring.initialize();
 
 	// std::queue<Command*> commandQueue;
 
-	// commandQueue.push(new SequentialCommandGroup(std::queue<Command*>({
-	// 	new DriveDistance(tankdrive, odom, 30, 2000),
-	// 	new TurnToHeading(tankdrive, odom, 135.0, 2000),
-		
-	// 	// new DriveDistance(tankdrive, odom, 12, 2000),
-		
-	// 	// new EngageIntake(intakeLeft, intakeRight),
-	// 	// new RunIntake(intake, centerStageLower, centerStageUpper,1000),
-	// 	// new DisengageIntake(intakeLeft, intakeRight),
-	// 	// new DriveDistance(tankdrive, odom, -26, 2000),
-
-	// 	// new RunScoring(intake, centerStageLower, centerStageUpper, hood, 2000),
-
-	// 	// new DriveDistance(tankdrive, odom, 26, 2000),
-	// 	// new SortOutIntake(intake, centerStageLower, 2000),
-
-
-	// 	// new RunIntake(intake, centerStageLower, centerStageUpper,2000),
-	// 	// new DisengageIntake(intakeLeft, intakeRight),
-	// 	// new DriveDistance(tankdrive, odom, -26, 2000),
-	// 	// new TimeoutCommand(200),
-	// 	// new RunScoring(intake, centerStageLower, centerStageUpper, hood, 2000)
-
-
-	// 	// // Drive to the loader
-	// 	// new TimeoutCommand(500),
-	// 	// new DriveDistance(tankdrive, odom, 26.0, 2000),
-	// 	// new TurnToHeading(tankdrive, odom, 90.0, 2000),
-
-	// 	// // Intake from the loader
-	// 	// new DriveDistance(tankdrive, odom, 12.0, 2000),
-	// 	// new EngageIntake(intakeLeft, intakeRight), 
-	// 	// new RunIntake(intake, centerStageLower, centerStageUpper,2000),
-
-	// 	// // Drive to scoring zone
-	// 	// new DisengageIntake(intakeLeft, intakeRight),
-	// 	// new DriveDistance(tankdrive, odom, -26.0, 1000),
-
-	// 	// new TimeoutCommand(2000),
-
-	// 	// // Score
-	// 	// new RunScoring(intake, centerStageLower, centerStageUpper, hood, 2000),
-
-	// 	// // Intake more, discard opposite color 
-	// 	// new EngageIntake(intakeLeft, intakeRight), 
-	// 	// new DriveDistance(tankdrive, odom, 5.0, 500),
-	// 	// new SortOutIntake(intake, centerStageLower, 2000),
-
-	// 	// // Wait and intake again
-	// 	// new TimeoutCommand(1000),
-	// 	// new RunIntake(intake, centerStageLower, centerStageUpper, 2000),
-
-	// 	// // Drive to scoring zone
-	// 	// new DisengageIntake(intakeLeft, intakeRight),
-	// 	// new DriveDistance(tankdrive, odom, -26.0, 1000),
-
-	// 	// // Score
-	// 	// new RunScoring(intake, centerStageLower, centerStageUpper, hood, 2000),
-
-	// 	// // Intake again
-	// 	// new EngageIntake(intakeLeft, intakeRight),
-	// 	// new DriveDistance(tankdrive, odom, 5.0, 500),
-	// 	// new RunIntake(intake, centerStageLower, centerStageUpper, 2000),
-
-	// 	// // Drive to scoring zone
-	// 	// new DisengageIntake(intakeLeft, intakeRight),
-	// 	// new DriveDistance(tankdrive, odom, -26.0, 1000),
-
-	// 	// // Score
-	// 	// new RunScoring(intake, centerStageLower, centerStageUpper, hood, 2000)
-	// })));
+	// commandQueue.push(new DriveDistance(tankdrive, odom, 24.0));
+	// commandQueue.push(new TurnToHeading(tankdrive, odom, 90.0));
+	// commandQueue.push(new DriveDistance(tankdrive, odom, 24.0));
+	// commandQueue.push(new TurnToHeading(tankdrive, odom, 180.0));
+	// commandQueue.push(new DriveDistance(tankdrive, odom, 24.0));
+	// commandQueue.push(new TurnToHeading(tankdrive, odom, 270.0));
+	// commandQueue.push(new DriveDistance(tankdrive, odom, 24.0));
+	// commandQueue.push(new TurnToHeading(tankdrive, odom, 0.0));
 
 	// CommandRunner commandRunner(commandQueue);
 	// commandRunner.run();
 
-	while (true) {
-		pros::delay(50);
-	}
+	// while (true) {
+	// 	pros::delay(50);
+	// }
 }
